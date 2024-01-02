83cd9aee78f5acc7e587e43927e9382be88f59d4
Sinh <phamtiensinh010@gmail.com>
2024-01-02 16:44:51 +0700

    implement basic main page and welcome page

---
+++ tsm-react-app/src/WelcomePage.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
+export const WelcomePage: React.FC = () => {
+
+  return (
+    <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+      <Col className="d-flex flex-column align-items-center">
+        <Row>Welcome!</Row>
+        <Row>Welcome To Task Management System!</Row>
+        <Row>
+          <Button href='/login'>Login</Button>
+        </Row>
+      </Col>
+    </Container>
+  );
+};
