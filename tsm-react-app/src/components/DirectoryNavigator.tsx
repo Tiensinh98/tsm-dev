c7498a413ceae65af01733e10f4ce06b609022c7
Sinh <phamtiensinh010@gmail.com>
2024-01-04 01:41:03 +0700

    implement directory navigator component

---
+++ tsm-react-app/src/components/DirectoryNavigator.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+
+interface DirectoryProps {
+  dir: string;
+  href: string;
+};
+
+
+interface DirectoryNavigatorProps {
+  directories: DirectoryProps[];
+};
+
+
+export const DirectoryNavigator: React.FC<DirectoryNavigatorProps> = (props) => {
+  const { directories } = props;
+
+  return (
+    <div role="presentation">
+      <Breadcrumbs aria-label="breadcrumb">
+        {directories.map((directory, index) => 
+          <Link 
+            underline="hover" 
+            color={index === directories.length - 1 ? "text.primary" : "inherit"}
+            href={directory.href}
+            aria-current={index === directories.length - 1 ? "page" : undefined}
+          >
+          {directory.dir}
+          </Link>)
+        }
+      </Breadcrumbs>
+    </div>
+  );
+}
